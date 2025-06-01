8159177f10414a77a250584aa0323867f0a99417
RM S <mixtop256@gmail.com>
2025-06-01 14:29:43 +0700

    Update screen again

--- src/Open.jsx
+++ src/Open.jsx
@@ -6,14 +6,7 @@ function Open() {
         <Fragment>
         <body className="Page-Main">
         <Link to="/lembar1" viewTransition>
-        <img src="https://i.pinimg.com/originals/d2/4d/48/d24d48006c777d53873392cb2b3b6f14.gif" style={
-            {   
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-            }
-        }></img>
+        <img className="Envlope" src="https://i.pinimg.com/originals/d2/4d/48/d24d48006c777d53873392cb2b3b6f14.gif"></img>
         </Link>
         </body>
         </Fragment>
