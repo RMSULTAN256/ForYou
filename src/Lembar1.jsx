609f82864c190c666c6297231a2ae071d1d3c33b
RM S <mixtop256@gmail.com>
2025-06-02 09:52:48 +0700

    Update my-lo

--- src/Lembar1.jsx
+++ src/Lembar1.jsx
@@ -1,26 +1,83 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 
 function Lembar1() {
+
+    const [Text, setText] = useState("Jadi, Kamu mau melanjutkan percakapan ini?");
+    const [denyCount, setDenyCount] = useState(0);
+    const [accCount, setAccCount] = useState(0);
+    const [isAccepted, setIsAccepted] = useState(false);
+    const [isFinal, setIsFinal] = useState(false);
+
+    const handleLanjut = () => {
+        setText("Ya... aku juga suka pada mu, jadi hmmmm~ Jadian yuk?");
+        setIsAccepted(true);
+        setDenyCount(0);
+        setAccCount(accCount + 1);
+        if (accCount >= 1) {
+            setText("Dengan ini kita resmi jadian ya, sayangku~💘. Anjay😏");
+            setIsFinal(true);
+        }
+    };
+
+    const handleGak = () => {
+        if (!isAccepted) {
+            const responseDeny = [
+            "Please dong, aku mau ngomong sesuatu",
+            "Please Dong~~~",
+            "PLEASEEEEEEE!!!!",
+            "Aku serius nih, please dong🥲",
+            "Aku cuma mau ngomong sesuatu yang penting"
+        ];
+        setText(responseDeny[Math.min(denyCount, responseDeny.length - 1)]);
+        setDenyCount(denyCount + 1);
+        } else {
+            const responsesAfter = [
+            "Ooops, tidak segampang itu bilang tidak, hehe",
+            "Hmmm, tidak bisa tidak dong, hehe",
+            "Eh, tidak bisa tidak dong, hehe",
+            "Bilang iya babe~💘",
+            "Mau apa hayo? tekan IYA"
+            ]
+            setText(responsesAfter[Math.min(denyCount, responsesAfter.length - 1)]);
+            setDenyCount(denyCount + 1);
+        }
+    }
+
+    const resbtn = [
+        "Iya gak?",
+        "Tombolnya ku matiin, hehe~",
+    ]
     return (
         <>
-        <body className='Lembaran' style={
-            {
-                backgroundImage: "url('https://images.unsplash.com/photo-1585146005358-05e9559ad597?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-                height: '100vh',
-            }
-        }>
-        </body>
-        <div className="Div-Container">
+        <div className="Container-Div">
             <div className="Text">
-            <p className="Isi">Apakah kamu ingin melanjutkan?</p>
-        </div>
-        <div className="Lembar1">
-            <button className="By">Lanjut</button>
-            <button className="Bn">Gak</button>
-        </div>
+                <p>{Text}</p>
+            </div>
+            <div className="Lembar1">
+                <button className="By" onClick={handleLanjut} style={{
+                    width: isFinal ? "75%" : "",
+                    height: isFinal ? "40%" : "",
+                    top: isFinal ? "50%" : "",
+                    left: isFinal ? "50%" : "",
+                    fontSize: isFinal ? "1.5rem" : "",
+                }}>{isFinal ? "Makasih ya Sayang💓💓😘" : "Boleh"}</button>
+                
+                {!isFinal && (
+                    <button 
+                        className={`Bn ${isAccepted ? "disabled" : ""}`} 
+                        onClick={handleGak}
+                        disabled={denyCount > 5}
+                        style={{
+                            opacity: denyCount > 5 ? 0.8 : 1,
+                            pointerEvents: denyCount > 5 ? "none" : "auto",
+                            cursor: denyCount > 5 ? "not-allowed" : "pointer",
+                            height: denyCount >= 5 ? "auto" : "",
+                        }}
+                    >
+                        {denyCount >= 5 && isAccepted ? "Hayoloh Tombolnya kumatiin lagi,, hehe~" : denyCount >= 5 ? `${resbtn[denyCount - 5]}`|| resbtn[resbtn.length - 1] : "Gak mau"}
+                    </button>
+                )}
+            </div>
         </div>
         
         </>
